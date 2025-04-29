1043fe53b2019ecf806c72f0031dea70084682aa
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2025-04-29 11:47:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ file = st.file_uploader("Upload file", type=["csv"])
 if file is not None:
   df = pd.read_csv(file)
   n_rows = st.slider('Choose number of rows to display', min_value=5, max_value=len(df), step=1)
-  st.write(df[:n_rows])
+  columns_to_show = st.multiselect("Select columns to show", df.columns.to_list(), default=df.columns.to_list())  
+  st.write(df.loc[:n_rows,columns_to_show])
           
