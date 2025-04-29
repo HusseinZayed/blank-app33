8969edf4327ee51519df5fc697629229a60f55ef
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2025-04-29 00:52:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("new app")
+shape = st.selectbox('Choose shape',['Circle','rectangle'])
 if shape=='Circle':
     r = st.number_input('Reduis: ',1.0,step=.1,max_value=10.0)
     area = 3.14*2*r
