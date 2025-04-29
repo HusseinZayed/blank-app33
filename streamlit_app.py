7b5e14aa8dd4f581cf33fa1efaeb312d2908f762
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2025-04-29 00:51:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+if shape=='Circle':
+    r = st.number_input('Reduis: ',1.0,step=.1,max_value=10.0)
+    area = 3.14*2*r
+else:
+    w = st.number_input('width: ',1.0,step=.1,max_value=10.0)
+    h = st.number_input('Height: ',1.0,step=.1,max_value=10.0)
+    area = w*h
+
+show_btn = st.button('show area')
+if show_btn:
+    st.write(area)
